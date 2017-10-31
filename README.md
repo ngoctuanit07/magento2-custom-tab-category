080b260787235997170cb8023dfdf03b042db3c0
php-cuong <bestearnmoney87@gmail.com>
2017-10-31 14:29:38 +0700

    init source code

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # magento2-custom-tab-category
 new tab in the category edit page in the backend
+
+# See the video about this tutorial here
+https://www.youtube.com/watch?v=xmpFEzKfiqA
---
+++ registration.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @Author: Ngo Quang Cuong
+ * @Date:   2017-10-31 14:11:26
+ * @Last Modified by:   https://www.facebook.com/giaphugroupcom
+ * @Last Modified time: 2017-10-31 14:11:33
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'PHPCuong_CustomTabCategory',
+    __DIR__
+);
